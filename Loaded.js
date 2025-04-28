1452051a6d2267cdbadbd2540bc281dadff18afd
unknown <konstantinos.bessas@unipv.it>
2025-04-28 21:18:33 +0200

    2025-04-28

--- Loaded.js
+++ Loaded.js
@@ -4,5 +4,9 @@ function Loaded() {
     var list = document.getElementsByTagName('hr');
     for (let item of list) {
       item.classList.remove("unloaded");
-    } 
+    }
+    var list2 = document.getElementsByClassName('fade-in');
+    for (let item of list2) {
+      item.classList.remove("unloaded");
+    }  
   }
